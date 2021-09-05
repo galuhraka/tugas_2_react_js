b42d237409db9751fd700b60758763695dc25414
tarripewe <tarripewe01@gmail.com>
2021-09-05 22:35:48 +0700

    tugas_3_react_js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Header from "./Page/Header";
 import Footer from "./Page/Footer";
-import MenuUtama from "./ListData/MenuUtama";
 import TentangKami from "./Page/TentangKami";
 import MenuKontak from "./Page/MenuKontak";
-import ListMakanan from "./ListData/ListMakanan";
-import MenuMakanan from "./ListData/MenuMakanan";
+import MenuMakanan from "./Page/MenuMakanan";
 
 function App() {
   return (
     <div>
       <Header />
-      <MenuUtama />
       <MenuMakanan />
       <TentangKami />
       <MenuKontak />
--- src/Page/Footer.js
+++ src/Page/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TopEvents from "../Events/TopEvents";
 
 function Footer() {
   return (
     <div id="container_footer">
-      <h5>Tugas 2 React Js</h5>
+      <TopEvents />
+      <h5>Tugas 3 React Js</h5>
     </div>
   );
 }
